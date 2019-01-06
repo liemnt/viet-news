0378b48386b503e287bd70f8d6030f32e60437c0
Unknown <liemnt.uit@gmail.com>
2019-01-06 19:44:35 +0700

    config deploy

--- viet-news/config.js
+++ viet-news/config.js
@@ -1,7 +1,10 @@
-var HOST = 'http://localhost:5006';
+var HOST = 'http://admin.news-system.me';
 var EXTENDED_PATH = '/api/v1';
-var DOMAIN = '//a.com';
-var BASE_URL = 'http://localhost:3000/api/';
+var DOMAIN = 'http://news-system.me';
+var BASE_URL = 'http://news-system.me/api/';
+
+// var DOMAIN = 'http://localhost:3000';
+// var BASE_URL = 'http://localhost:3000/api/';
 
 module.exports.HOST = HOST;
 module.exports.EXTENDED_PATH = EXTENDED_PATH;
